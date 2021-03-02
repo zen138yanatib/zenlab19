b0fda789cc911f01934919281073d5fea3c9e68d
zen138yanatib <64641188+zen138yanatib@users.noreply.github.com>
2021-03-02 15:46:30 +0700

    Update lab19.h

--- lab19.h
+++ lab19.h
@@ -63,13 +63,46 @@ void Unit::newTurn(){
 	guard_on = false;
 }
 
+bool Unit::isDead(){
+    if(hp <= 0) return true;
+    else{
+        return false;
+    }
+}
 
+void Unit::guard(){
+    guard_on = true;
+}
 
-/////////////////////////////////////////////////////////////////////////////////////
-//Write function members isDead(), guard(), heal(), beAttacked(), and attack() here//
-/////////////////////////////////////////////////////////////////////////////////////
+int Unit::beAttacked(int oppatk){
+    int dmg;
+    if(guard_on == false){
+        dmg = oppatk - def;
+        hp -= dmg;
+    }
+    else if(guard_on == true){
+        dmg = (oppatk - def)/3;
+        hp -= dmg;
+    }
+    return dmg;
+}
 
+int Unit::attack(Unit &target){
+	return target.beAttacked(atk);
+}
 
+int Unit::heal(){
+    int he = 0, oldhp;
+    oldhp = hp;
+    he = rand()%21+10;
+    hp += he;
+    if(hp > hpmax){
+        int diff = hpmax - oldhp;
+        hp = hpmax;
+        return diff;
+    }
+    else return he;
+}
 
 void drawScene(char p_action,int p,char m_action,int m){
 	cout << "                                                       \n";
